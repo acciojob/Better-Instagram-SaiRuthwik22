5430ee91c4724376b4b774321c84ceabe8d44cf2
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-15 11:40:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+let dragEle = null;
+let containerEle=null;
+function started(event) {
+	dragEle = event.target
+}
+function allowDrag(event){
+	event.preventDefault()
+}
+function ending(event) {
+	event.preventDefault()
+	let someId = dragEle.id
+	let someC = dragEle.textContent
+	let containerEle = event.target
+	let x = document.getElementById(event.target.id)
+	let y = document.getElementById(dragEle.id)
+	y.id = containerEle.id
+	y.textContent = containerEle.textContent
+	x.id= someId
+	x.textContent = someC
+	dragEle = null
+	containerEle=null
+	someC=null
+	someId=null
+}
+
