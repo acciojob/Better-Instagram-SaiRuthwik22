23fde37fd0329da30e90902f2df23c032a8a998a
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-22 18:47:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-//your code here
 let dragEle = null;
-let containerEle=null;
+
 function started(event) {
-	dragEle = event.target
-}
-function allowDrag(event){
-	event.preventDefault()
-}
-function ending(event) {
-    event.preventDefault();
-    let containerEle = event.target;
-    let sourceImage = window.getComputedStyle(dragEle).backgroundImage;
-    let targetImage = window.getComputedStyle(containerEle).backgroundImage;
-    dragEle.style.backgroundImage = targetImage;
-    containerEle.style.backgroundImage = sourceImage;
-    dragEle = null;
+	dragEle = event.target;
 }
 
+function allowDrag(event){
+	event.preventDefault();
+}
+
+function ending(event) {
+	event.preventDefault();
+	let containerEle = event.target;
+	
+	// Get the background images of the source and target divs
+	let sourceImage = window.getComputedStyle(dragEle).backgroundImage;
+	let targetImage = window.getComputedStyle(containerEle).backgroundImage;
+
+	dragEle.style.backgroundImage = targetImage;
+	containerEle.style.backgroundImage = sourceImage;
+	
+	dragEle = null;
+}
